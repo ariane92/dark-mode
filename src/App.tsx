c362a6b1029b214603142b7cb27463bd25a67df6
Ariane Mateus <ariane.ccomp@gmail.com>
2020-07-25 18:01:23 -0300

    feat: change theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {ThemeProvider} from 'styled-components';
 
-function App() {
+import {darkTheme, lightTheme} from './theme';
+import { Container, SwitchButton, Title, User, Subtitle } from './styles'
+
+const App: React.FC = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    if(theme === 'light'){
+      localStorage.setItem("theme", "dark");
+      setTheme("dark")
+    }else {
+      localStorage.setItem("theme", "light");
+      setTheme("light");
+    }
+
+    
+  };
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    localTheme && setTheme(localTheme);
+  }, []);
+
   return (
-    <h1>Hello</h1>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <Container>
+        <Title>{theme} mode</Title>
+        <SwitchButton onClick={() => toggleTheme()}>
+          {theme === 'dark' ? (
+            <img src="./src/assets/sun.svg" />
+          ) :( <img src="./src/assets/moon.svg" />)}
+        </SwitchButton>
+        <Subtitle className="mt4"> by: <User href="" target="_black" className="ml2">@arianemateus
+
+        </User>
+
+        </Subtitle>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const lightTheme  = {
+    background: '#f6f8fd',
+    text: '#282a36'
+};
+
+export const darkTheme = {
+    background: "#282a36",
+    text: "#f6f8fd",
+  };
